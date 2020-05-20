c7a05a4007c37b872eb76b54908ab63999f3923b
James B Schoen <edensongjbs@gmail.com>
2020-05-20 15:41:35 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,7 +18,7 @@ def play(songs)
   user_input=gets.strip
   if (user_input.to_i > 0) && (user_input.to_i <= songs.length)
     puts "Playing #{songs[user_input.to_i-1]}"
-  elsif songs.includes?(user_input)
+  elsif songs.include?(user_input)
     puts "Playing #{user_input}"
   else
     play(songs)
