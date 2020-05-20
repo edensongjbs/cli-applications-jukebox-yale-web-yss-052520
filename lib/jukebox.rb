09b31446ad8aa68d5e6ecec5fcc304cfb6538761
James B Schoen <edensongjbs@gmail.com>
2020-05-20 15:29:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,17 +11,17 @@ def list(songs)
   for i in 0..songs.length {puts "#{i+1}. #{songs[i]}"}
 end
   
-def play(songs)
-  puts "Please enter a song name or number"
-  user_input=gets.strip
-  if (user_input.to_i > 0) && (user_input.to_i <= songs.length)
-    puts "Playing #{songs[user_input.to_i-1]}"
-  elsif songs.includes?(user_input)
-    puts "Playing #{user_input}"
-  else
-    play(songs)
-  end
-end
+#def play(songs)
+#  puts "Please enter a song name or number"
+#  user_input=gets.strip
+#  if (user_input.to_i > 0) && (user_input.to_i <= songs.length)
+#    puts "Playing #{songs[user_input.to_i-1]}"
+#  elsif songs.includes?(user_input)
+#    puts "Playing #{user_input}"
+#  else
+#    play(songs)
+#  end
+#end
 
 def exit_jukebox
   puts "Goodbye"
